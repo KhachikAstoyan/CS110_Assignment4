facd6a30b9e4d1ac7992baeb80bd51c322c25c83
Khachik <khachik.astoyan228@gmail.com>
2022-11-16 19:31:29 +0400

    modify a condition in toHex.js

--- ex2/toHex.js
+++ ex2/toHex.js
@@ -8,8 +8,7 @@ module.exports = function toHex(num) {
   while (quotient !== 0) {
     remainder = quotient % 16;
     quotient = Math.floor(quotient / 16);
-    if (remainder <= 9) hex.push(remainder);
-    else hex.push(chars[remainder - 10]);
+    hex.push(remainder <= 9 ? remainder : chars[remainder - 10]);
   }
 
   return hex.reverse().join("");
